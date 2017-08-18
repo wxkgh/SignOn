f46b6676d3bc68eb31148efdbb20174f898ed932
wangxk <wxkskynet@vip.qq.com>
2017-08-18 23:49:10 +0800

    用户登录验证成功

--- task/src/main/java/org/life/task/Controller/signin.java
+++ task/src/main/java/org/life/task/Controller/signin.java
@@ -1,14 +1,18 @@
 package org.life.task.Controller;
 
 import org.life.task.DAO.UserDao;
+import org.life.task.Model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @Controller
 public class signin {
@@ -22,11 +26,20 @@ public class signin {
     }
 
     @RequestMapping("/signin")
-    public void signin(HttpServletRequest request, HttpServletResponse response) {
+    public void signin(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        int status;
         String username = request.getParameter("username");
         String password = request.getParameter("password");
+        User currentUser = userDB.findUser(username, password);
+        if (currentUser == null) {
+            status = -1;
+        } else {
+            status = 0;
+        }
 
-
-
+        PrintWriter pw = response.getWriter();
+        pw.printf("{\"status\":%d}", status);
+        response.setContentType("text/json");
     }
+
 }
