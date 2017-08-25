40034c40b023e6c12eb51fdeb5de8b3d9f920e2f
wangxk <wxkskynet@vip.qq.com>
2017-08-25 22:05:13 +0800

    增加SSL支持

--- src/main/java/org/skynet/web/WebApplication.java
+++ src/main/java/org/skynet/web/WebApplication.java
@@ -1,12 +1,34 @@
 package org.skynet.web;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.SocketUtils;
 
 @SpringBootApplication
 public class WebApplication {
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(WebApplication.class, args);
 	}
+
+    @Bean
+    public Integer port() {
+        return SocketUtils.findAvailableTcpPort();
+    }
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(port());
+        return connector;
+    }
 }
