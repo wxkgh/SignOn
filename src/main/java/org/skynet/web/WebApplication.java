936c5eba29026dc49aed0003c00d06fee734ae3d
wangxk <wxkskynet@vip.qq.com>
2017-08-26 14:01:26 +0800

    http重定向到https

--- src/main/java/org/skynet/web/WebApplication.java
+++ src/main/java/org/skynet/web/WebApplication.java
@@ -1,6 +1,10 @@
 package org.skynet.web;
 
 import org.apache.catalina.connector.Connector;
+import org.apache.catalina.Context;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
@@ -10,6 +14,9 @@ import org.springframework.util.SocketUtils;
 
 @SpringBootApplication
 public class WebApplication {
+    @Value("${server.port}")
+    private int port;
+
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(WebApplication.class, args);
 	}
@@ -21,14 +28,27 @@ public class WebApplication {
 
     @Bean
     public EmbeddedServletContainerFactory servletContainer() {
-        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
         tomcat.addAdditionalTomcatConnectors(createStandardConnector());
         return tomcat;
     }
 
     private Connector createStandardConnector() {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setPort(port());
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(port);
         return connector;
     }
 }
