56109144157f26a54c6918af0f03eeb47a307531
wangxk <wxkskynet@vip.qq.com>
2017-08-27 15:52:24 +0800

    测试用户登录
    加密密码的保存和验证

--- src/main/java/org/skynet/web/Controller/UserController.java
+++ src/main/java/org/skynet/web/Controller/UserController.java
@@ -1,12 +1,21 @@
 package org.skynet.web.Controller;
 
+import com.sun.deploy.net.HttpResponse;
+import org.skynet.web.Helper.MD5Helper;
+import org.skynet.web.Mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 
 @Controller
 public class UserController {
+
+    @Autowired
+    UserMapper userMapper;
 
     @RequestMapping("/")
     public String index() {
@@ -18,9 +27,50 @@ public class UserController {
         return "LogIn";
     }
 
+    @RequestMapping("SignIn")
+    public void singIn(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String md5pswd = MD5Helper.encodeByMd5(password);
+
+        int status;
+        if (userMapper.findUserByAccount(username, md5pswd) != null) {
+            status = 0;
+        } else {
+            status = -1;
+        }
+
+        PrintWriter pw = response.getWriter();
+        pw.printf("{\"status\":%d}", status);
+        response.setContentType("text/json");
+    }
+
+    @RequestMapping("AddUser")
+    public void addUser(HttpServletResponse response, HttpServletRequest request) throws Exception {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String md5pswd = MD5Helper.encodeByMd5(password);
+
+        int status;
+        if (userMapper.insertUserByAccount(username, md5pswd) == 1) {
+            status = 0;
+        } else {
+            status = -1;
+        }
+
+        PrintWriter pw = response.getWriter();
+        pw.printf("{\"status\":%d}", status);
+        response.setContentType("text/json");
+    }
+
     @RequestMapping("Register")
-    public String register() {
+    public String Register() {
         return "Register";
+    }
+
+    @RequestMapping("UserIndex")
+    public String userIndex() {
+        return "UserIndex";
     }
 
 //    @RequestMapping(value = "/{reader}", method = RequestMethod.GET)
